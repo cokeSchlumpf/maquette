1ec2995cf96c09778d0ca9c1fadf7a4ff5002391
Michael Wellner <michael.wellner@de.ibm.com>
2019-11-13 00:39:32 +0100

    Created form ... integration missing.

--- web-ui/src/components/elements/AccessRequestForm/index.js
+++ web-ui/src/components/elements/AccessRequestForm/index.js
@@ -1,10 +1,42 @@
-import React from 'react';
+import _ from 'lodash';
+import React, { useEffect, useState } from 'react';
 import './styles.scss';
 
 import ContentSection from '../ContentSection';
-import { Form, FormGroup, RadioButtonGroup, RadioButton, TextArea } from 'carbon-components-react';
+import { Button, Form, FormGroup, RadioButtonGroup, RadioButton, TextArea } from 'carbon-components-react';
 
-export default () => {
+const onSubmitDefault = (value) => {
+    console.log(value);
+};
+
+export default ({ onSubmit = onSubmitDefault }) => {
+
+    const defaultValue = { authorization: "consumer", justification: "" };
+    const [value, setValue] = useState(defaultValue);
+    const [justificationInvalid, setJustificationInvalid] = useState(false);
+
+    const onChange = (field, fieldValue) => {
+        setValue(_.assign(value, { [field]: fieldValue }))
+    };
+
+    const onChangeInput = (field) => (event) => {
+        onChange(field, event.target.value);
+    };
+
+    const onChangeRadioGroup = (value, field, event) => {
+        onChange(field, value);
+    };
+
+    const onSubmitButtonClick = (event) => {
+        event.preventDefault();
+
+        if (value.justification.length === 0) {
+            setJustificationInvalid(true);
+        } else {
+            onSubmit(value);
+        }
+    };
+
     return (
         <>
             <h3 className="mq--cards-heading">Request Access</h3>
@@ -14,7 +46,8 @@ export default () => {
                         <RadioButtonGroup
                             name="authorization"
                             id="authorization"
-                            defaultSelected="consumer">
+                            defaultSelected={ defaultValue.authorization }
+                            onChange={ onChangeRadioGroup }>
 
                             <RadioButton value="consumer" id="consumer" labelText="consumer" />
                             <RadioButton value="producer" id="producer" labelText="producer" />
@@ -22,7 +55,17 @@ export default () => {
                         </RadioButtonGroup>
                     </FormGroup>
 
-                    <TextArea labelText="Justification" helperText="Enter a justification for your request" />
+                    <FormGroup legendText="">
+                        <TextArea
+                            labelText="Justification"
+                            helperText="Enter a justification for your request"
+                            defaultValue={ defaultValue.justification }
+                            invalid={ justificationInvalid }
+                            invalidText="Justification must not be empty!"
+                            onChange={ onChangeInput("justification") } />
+                    </FormGroup>
+
+                    <Button type="submit" onClick={ onSubmitButtonClick }>Submit Request</Button>
                 </ContentSection>
             </Form>
         </>);
